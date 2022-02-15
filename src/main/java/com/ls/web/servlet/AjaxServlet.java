ecacfe30fea298d6b1f37b5340b9bc25f407fbed
liang <185378131@qq.com>
2022-02-15 18:08:17 +0800

    push test new new new new

--- src/main/java/com/ls/web/servlet/AjaxServlet.java
+++ src/main/java/com/ls/web/servlet/AjaxServlet.java
@@ -20,6 +20,7 @@ public class AjaxServlet extends HttpServlet {
         System.out.println("hello 4087");
         System.out.println("push test");
         System.out.println("push test new");
+        System.out.println("push test new new new");
 
 
     }
